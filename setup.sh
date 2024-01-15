4672d0b203e7a0edad510fce087c3d54fd6e1e7f
linrongbin16 <linrongbin16@outlook.com>
2024-01-15 14:15:02 +0800

    chore

--- setup.sh
+++ setup.sh
@@ -37,33 +37,32 @@ echo "# $REPO" >README.md
 info "clear README.md - done"
 
 info "replace 'linrongbin16' to '$ORG' in LICENSE"
-sed "s/linrongbin16/""$ORG""/g" LICENSE >.tmp.LICENSE
+sed -i -e "s#linrongbin16#$ORG#g" LICENSE
 mv .tmp.LICENSE LICENSE
 info "replace 'linrongbin16' to '$ORG' in LICENSE - done"
 
 info "replace 'ci-template.nvim' to '$REPO' in LICENSE"
-sed "s/ci-template.nvim/""$REPO""/g" LICENSE >.tmp.LICENSE
-mv .tmp.LICENSE LICENSE
+sed -i -e "s#ci-template.nvim#$REPO#g" LICENSE
 info "replace 'ci-template.nvim' to '$REPO' in LICENSE - done"
 
 info "replace 'linrongbin16' to '$ORG' in .github/workflows"
-sed -i "s/linrongbin16/$ORG/" .github/workflows/lint.yml
-sed -i "s/linrongbin16/$ORG/" .github/workflows/release.yml
-sed -i "s/linrongbin16/$ORG/" .github/workflows/test.yml
+sed -i -e "s#linrongbin16#$ORG#g" .github/workflows/lint.yml
+sed -i -e "s#linrongbin16#$ORG#g" .github/workflows/release.yml
+sed -i -e "s#linrongbin16#$ORG#g" .github/workflows/test.yml
 info "replace 'linrongbin16' to '$ORG' in .github/workflows - done"
 
 info "replace 'ci-template.nvim' to '$REPO' in .github/workflows"
-sed -i "s/ci-template.nvim/$REPO/" .github/workflows/lint.yml
-sed -i "s/ci-template.nvim/$REPO/" .github/workflows/release.yml
-sed -i "s/ci-template.nvim/$REPO/" .github/workflows/test.yml
+sed -i -e "s#ci-template.nvim#$REPO#g" .github/workflows/lint.yml
+sed -i -e "s#ci-template.nvim#$REPO#g" .github/workflows/release.yml
+sed -i -e "s#ci-template.nvim#$REPO#g" .github/workflows/test.yml
 info "replace 'ci-template.nvim' to '$REPO' in .github/workflows - done"
 
 info "replace 'linrongbin16' to '$ORG' in .luacov"
-sed -i "s/linrongbin16/$ORG/" .luacov
+sed -i -e "s#linrongbin16#$ORG#g" .luacov
 info "replace 'linrongbin16' to '$ORG' in .luacov - done"
 
 info "replace 'ci-template' to '$REPO' in .luacov"
-sed -i "s/ci-template/$REPO/" .luacov
+sed -i -e "s#ci-template#$REPO#g" .luacov
 info "replace 'ci-template' to '$REPO' in .luacov - done"
 
 info "rename lua/ci-template.lua to lua/$REPO"
@@ -71,12 +70,12 @@ mv lua/ci-template.lua lua/$REPO.lua
 info "rename lua/ci-template.lua to lua/$REPO - done"
 
 info "replace 'ci-template' to '$REPO' in .luacov"
-sed -i "s/ci-template/$REPO/" spec/ci_template_spec.lua
+sed -i -e "s#ci-template#$REPO#g" spec/ci_template_spec.lua
 info "replace 'ci-template' to '$REPO' in .luacov - done"
 
 REPO_SPEC="${REPO/-/_}"
 info "replace 'ci_template' to '$REPO_SPEC' in spec/ci_template_spec.lua"
-sed -i "s/ci_template/$REPO_SPEC/" spec/ci_template_spec.lua
+sed -i "s#ci_template#$REPO_SPEC#g" spec/ci_template_spec.lua
 info "replace 'ci_template' to '$REPO_SPEC' in spec/ci_template_spec.lua - done"
 
 REPO_SPEC_LUA="spec/${REPO}_spec.lua"
@@ -94,7 +93,7 @@ if [[ -z "$INDENT" ]]; then
 fi
 
 info "replace indent size to '$INDENT'"
-sed -i "/^indent_size/ s/2/$INDENT/g" .editorconfig
-sed -i "/^indent_width/ s/2/$INDENT/g" .stylua.toml
-sed -i "s/2/$INDENT/g" .nvim.lua
+sed -i -e "#^indent_size# s#2#$INDENT#g" .editorconfig
+sed -i -e "#^indent_width# s#2#$INDENT#g" .stylua.toml
+sed -i -e "s#2#$INDENT#g" .nvim.lua
 info "replace indent size to '$INDENT' - done"
