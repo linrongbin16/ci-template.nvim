974b1877af3ef95bc8192fb69246eb312b62394a
linrongbin16 <linrongbin16@outlook.com>
2024-01-15 13:49:52 +0800

    chore

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ sed -i "s/linrongbin16/$ORG/g" LICENSE
 info "replace 'linrongbin16' to '$ORG' in LICENSE - done"
 
 info "replace 'ci-template.nvim' to '$REPO' in LICENSE"
-sed -i "s/ci\-template.nvim/$REPO/g" LICENSE
+sed -i "s/ci\\-template.nvim/$REPO/g" LICENSE
 info "replace 'ci-template.nvim' to '$REPO' in LICENSE - done"
 
 info "replace 'linrongbin16' to '$ORG' in .github/workflows"
@@ -51,9 +51,9 @@ sed -i "s/linrongbin16/$ORG/g" .github/workflows/test.yml
 info "replace 'linrongbin16' to '$ORG' in .github/workflows - done"
 
 info "replace 'ci-template.nvim' to '$REPO' in .github/workflows"
-sed -i "s/ci\-template.nvim/$REPO/g" .github/workflows/lint.yml
-sed -i "s/ci\-template.nvim/$REPO/g" .github/workflows/release.yml
-sed -i "s/ci\-template.nvim/$REPO/g" .github/workflows/test.yml
+sed -i "s/ci\\-template.nvim/$REPO/g" .github/workflows/lint.yml
+sed -i "s/ci\\-template.nvim/$REPO/g" .github/workflows/release.yml
+sed -i "s/ci\\-template.nvim/$REPO/g" .github/workflows/test.yml
 info "replace 'ci-template.nvim' to '$REPO' in .github/workflows - done"
 
 info "replace 'linrongbin16' to '$ORG' in .luacov"
@@ -61,7 +61,7 @@ sed -i "s/linrongbin16/$ORG/g" .luacov
 info "replace 'linrongbin16' to '$ORG' in .luacov - done"
 
 info "replace 'ci-template' to '$REPO' in .luacov"
-sed -i "s/ci\-template/$REPO/g" .luacov
+sed -i "s/ci\\-template/$REPO/g" .luacov
 info "replace 'ci-template' to '$REPO' in .luacov - done"
 
 info "rename lua/ci-template.lua to lua/$REPO"
@@ -69,14 +69,19 @@ mv lua/ci-template.lua lua/$REPO.lua
 info "rename lua/ci-template.lua to lua/$REPO - done"
 
 info "replace 'ci-template' to '$REPO' in .luacov"
-sed -i "s/ci\-template/$REPO/g" spec/ci_template_spec.lua
+sed -i "s/ci\\-template/$REPO/g" spec/ci_template_spec.lua
 info "replace 'ci-template' to '$REPO' in .luacov - done"
 
-REPO_SPEC="spec/${REPO}_spec.lua"
-REPO_SPEC="${REPO_SPEC/-/_}"
-info "rename lua/ci_template_spec.lua to '$REPO_SPEC'"
-mv spec/ci_template_spec.lua $REPO_SPEC
-info "rename lua/ci_template_spec.lua to '$REPO_SPEC' - done"
+REPO_SPEC="${REPO/-/_}"
+info "replace 'ci_template' to '$REPO_SPEC' in spec/ci_template_spec.lua"
+sed -i "s/ci_template/$REPO_SPEC/g" spec/ci_template_spec.lua
+info "replace 'ci_template' to '$REPO_SPEC' in spec/ci_template_spec.lua - done"
+
+REPO_SPEC_LUA="spec/${REPO}_spec.lua"
+REPO_SPEC_LUA="${REPO_SPEC_LUA/-/_}"
+info "rename lua/ci_template_spec.lua to '$REPO_SPEC_LUA'"
+mv spec/ci_template_spec.lua $REPO_SPEC_LUA
+info "rename lua/ci_template_spec.lua to '$REPO_SPEC_LUA' - done"
 
 info "clear version.txt"
 echo '' >version.txt
